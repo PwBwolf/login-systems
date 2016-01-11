0d7bae7ff11d5ceab1343968ba4e2f360a11ffcb
Peter Borodich <pwborodich@gmail.com>
2016-01-10 20:45:09 -0500

    mongoose middleware

--- api/models/userModel.js
+++ api/models/userModel.js
@@ -6,13 +6,40 @@
  */
 var mongoose = require('mongoose');
 var bcrypt = require('bcrypt-nodejs');
-
+//mongoose schema for a user 
 var userSchema = new mongoose.Schema({
   name: { type: String, required: true },// for each key value pair we are defining the type and if it is required to create user.
   email: { type: String, required: true },
   password: { type: String, required: true },
   admin: { type: Boolean }
-})
+});
+
+// encrypt the password before the user is saved to the mongodb
+userSchema.pre('save', function (next) {
+  var user = this;
+  //check to see if the password has been changed
+  if (!user.isModified('password')) {
+    return next();
+  }
+  // Make the SALT 
+  bcrypt.genSalt(10, function (err, salt) {
+    if (err) {
+      return next(err);
+    }
+    // Hash the password with the created salt
+    bycrypt.hash(user.password, salt, null, function (err, hash) {
+      if (err) {
+        return next(err);
+      }
+      //if there is no error save the new encrypted password 
+      user.password = hash;
+      next();
+    });
+        
+  }); 
+
+});
+
 
 
  
